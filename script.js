0b190fdfc057b697e21d05f8a5ea3cc10d9dce89
DiracSpace <www.ralphlaurenpolo@gmail.com>
2021-06-10 13:23:36 -0500

    Added regex expression validators for yt URL and yt URL ID extractor, plus whitespace validator for checking before trying to remove. Fixed UI design for more cleaner flex organization

--- script.js
+++ script.js
@@ -2,9 +2,21 @@ var url;
 var videoId;
 var embedURL;
 
+const ytURLValidator = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(\?\S*)?$/;
+const ytURLidExtract = /http(?:s)?:\/\/(?:m.)?(?:www\.)?youtu(?:\.be\/|be\.com\/(?:watch\?(?:feature=youtu.be\&)?v=|v\/|embed\/|user\/(?:[\w#]+\/)+))([^&#?\n]+)/;
+const whitespacesValidator = /\s/;
+
 function getVideoURL() {
     // gets our url and removes any whitespaces the user may have added
-    url = prompt("Insert the URL of the video you want to watch").replace(/\s/g, "");
+    url = prompt("Insert the URL of the video you want to watch");
+
+    // checks for whitespaces first, if true then removes
+    // because it threw null error
+    let hasWhitepaces = whitespacesValidator.test(url);
+    if (hasWhitepaces) {
+        url.replace(/\s/g, "");
+    }
+
     validateURL(url);
 }
 
@@ -13,21 +25,30 @@ function getVideoURL() {
 
 function validateURL(url) {
     // checks if link given is from youtube.com
-    if (url.includes("youtube.com/watch?v=") || url.includes("youtu.be/") && url.length >= 20) {
-        getId(url);
-    } else {
+    // doing this with regex validation for
+    // dynammic domains or other yt feature urls
+    let isYtFormatMatch = ytURLValidator.test(url);
+
+    if (!isYtFormatMatch) {
         alert("Invalid URL\nTry Again");
         refresh();
         getVideoURL();
     }
+
+    getId(url);
 }
 
 // strips the video id from our url
 function getId(url) {
+    // check url and always get ytID
+    let ytID = ytURLidExtract.exec(url)[1];
+
     // the video id is 11 characters long and is always at the end of the URL so we the the substring from the length minus 11
-    videoId = url.substr(url.length - 11, 11);
-    loadVideo(videoId);
-    return videoId;
+    // videoId = url.substr(url.length - 11, 11);
+
+    loadVideo(ytID);
+
+    return ytID;
 }
 
 function loadVideo(videoId) {
