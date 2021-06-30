d19a3e1981fbc42fa742d8c7d2c9a76594f0e5a7
UnrealApex <dojo1011234@gmail.com>
2021-06-30 02:57:41 +0000

    Added comments

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ var url;
 var videoId;
 var embedURL;
 
+// regular expressions used in the program, I highly suggest using regex101.com for a detailed explaination of the expression's inner workings 
+// gets the video id from the url inputted by the user
 const videoIdExtractor =
   /(http(?: s) ?: \/\/(?:m.)?(?:www\.)?)?youtu(?:\.be\/|be\.com\/(?:watch\?(?:feature=youtu\.be\&)?v=|v\/|embed\/|user\/(?:[\w#]+\/)+))([^&#?\n]+)/;
+// checks if the url is a valid youtube url and is something our player can play
 const urlValidator =
   /((https(?:s)?:\/\/)?(www\.)?)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?&v=))((?:\w|-){11})((?:\&|\?)\S*)?/;
+// expression to test if there are any whitspaces in our url
 const whiteSpaceValidator = /\s/g;
 function getVideoURL() {
-  // gets our url
+  // gets our url from a prompt
   url = prompt("Insert the URL of the video you want to watch");
   var hasWhiteSpace = whiteSpaceValidator.test(url);
   if (hasWhiteSpace) {
@@ -34,19 +38,22 @@ function validateURL(url) {
   }
 }
 
-// strips the video id from our url
+
 function getId(url) {
+  // strips the video id from our url
   videoId = videoIdExtractor.exec(url)[2];
   loadVideo(videoId);
   return videoId;
 }
 
 function loadVideo(videoId) {
+  // sets the video player iframe's url to a youtube embed url(explains why there are no video advertisements and why it is completely legal to opperate this website)
   embedURL = `https://www.youtube.com/embed/${videoId}`;
   document.getElementById("videoPlayer").src = embedURL;
 }
 
 function openFullscreen() {
+  // puts the play in full screen mode
   var player = document.getElementById("videoPlayer");
   if (player.src.length != 0) {
     if (player.requestFullscreen) {
@@ -67,11 +74,13 @@ function openFullscreen() {
 }
 
 function refresh() {
+  // resets the player if the user entered an invalid url or ran into another problem
   url = "";
   document.getElementById("videoPlayer").src = "";
 }
 
 function shareVideo() {
+  // copies shortend youtube url to the user's clipboard
   if (videoId != undefined) {
     navigator.clipboard.writeText("https://youtu.be/" + videoId);
     alert("Link copied to clipboard");
@@ -82,6 +91,7 @@ function shareVideo() {
 }
 
 function info() {
+  // help if the user is stuck or wants to get info
   alert(
     "Welcome to YT-Player! To use YT-Player, click the play icon to play a video, the full screen icon to put the video in full screen, and the reload icon to reset the player if something went wrong.\n\nMade with love by UnrealApex\nThank you to all those who gave feedback and helped improve this project"
   );
