2dac538f3d71f99eb5b4884e025e4201ff4185dd
Aaron <79121360+UnrealApex@users.noreply.github.com>
2021-07-04 19:45:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -132,36 +132,6 @@ function openVideoInNewTab() {
 
 function settings() {
   // TODO: use cookies to save preferances
-  var modal = document.getElementById("myModal");
+  let modal = document.getElementById("myModal");
   modal.style.display = "block";
-}
-
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  console.log("closed modal with x");
-  modal.style.display = "none";
-
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    console.log("closed modal ");
-    modal.style.display = "none";
-  }
 }
