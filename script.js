312ef98055df995a1e1e57c7fb45a7629dd32142
UnrealApex <dojo1011234@gmail.com>
2021-07-27 00:56:16 +0000

    Revert "Revert "Added simple ui""
    This reverts commit 60a7b36c0b0793466e9be72d0f1b8daf45a4fdbb.

--- script.js
+++ script.js
@@ -1,82 +1,111 @@
 var url;
 var videoId;
 var embedURL;
-
-const videoIdExtractor = /(http(?: s) ?: \/\/(?:m.)?(?:www\.)?)?youtu(?:\.be\/|be\.com\/(?:watch\?(?:feature=youtu\.be\&)?v=|v\/|embed\/|user\/(?:[\w#]+\/)+))([^&#?\n]+)/;;
-const urlValidator = /((https(?:s)?:\/\/)?(www\.)?)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?&v=))((?:\w|-){11})((?:\&|\?)\S*)?/;
+var modal = document.getElementById("modal");
+var player = document.getElementById("videoPlayer");
+const videoIdExtractor = /(http(?: s) ?: \/\/(?:m.)?(?:www\.)?)?youtu(?:\.be\/|be\.com\/(?:watch\?(?:feature=youtu\.be\&)?v=|v\/|embed\/|user\/(?:[\w#]+\/)+))([^&#?\n]+)/;
+const urlValidator = /((https(?:s):\/\/)?(www\.)?)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?&v=))((?:\w|-){11})((?:\&|\?)\S*)?/;
 const whiteSpaceValidator = /\s/g;
-function getVideoURL() {
-    // gets our url
-    url = prompt("Insert the URL of the video you want to watch");
-    var hasWhiteSpace = whiteSpaceValidator.test(url);
-    if (hasWhiteSpace) {
-        url.replace(/\s/g, "");
-    }
 
+function getVideoURL() {
+  // gets our url
+  url = document.getElementById("url").value;
+  let hasWhiteSpace = whiteSpaceValidator.test(url);
+  if (hasWhiteSpace) {
+    url.replace(/\s/g, "");
+  }
+  if (url) {
     validateURL(url);
+  } else {
+    refresh();
+  }
 }
 
 // TODO: add Vimeo support
 // TODO: add ability to play youtube playlists
 
 function validateURL(url) {
-    // checks if link given is from youtube.com using regex
-    var isValidURL = urlValidator.test(url);
-    if (!isValidURL && url.length != 0) {
-        alert("Invalid URL\nTry Again");
-        refresh();
-        getVideoURL();
-    } 
-    getId(url);
+  // checks if link given is from youtube.com using regex
+  var isValidURL = urlValidator.test(url);
+  if (!isValidURL && url.length != 0 && url != null) {
+    alert("Invalid URL\nTry Again");
+    refresh();
+    // getVideoURL();
+  }
+  getId(url);
 }
 
 // strips the video id from our url
 function getId(url) {
-    videoId = videoIdExtractor.exec(url)[2];
-    loadVideo(videoId);
-    return videoId;
+  videoId = videoIdExtractor.exec(url)[2];
+  loadVideo(videoId);
+  return videoId;
 }
 
 function loadVideo(videoId) {
-    embedURL = `https://www.youtube.com/embed/${videoId}`;
-    document.getElementById("videoPlayer").src = embedURL;
+  embedURL = `https://www.youtube.com/embed/${videoId}`;
+  document.getElementById("videoPlayer").src = embedURL;
+  modal.style.display = "block";
 }
 
 function openFullscreen() {
-    var player = document.getElementById("videoPlayer");
-    if (player.src.length != 0) {
-        if (player.requestFullscreen) {
-            player.requestFullscreen();
-        } else if (player.webkitRequestFullscreen) {
-            /* Safari */
-            player.webkitRequestFullscreen();
-        } else if (player.msRequestFullscreen) {
-            /* IE11 */
-            player.msRequestFullscreen();
-        } else {
-            alert("Unable to open video in full screen");
-        }
+  if (player.src.length != 0) {
+    if (player.requestFullscreen) {
+      player.requestFullscreen();
+    } else if (player.webkitRequestFullscreen) {
+      /* Safari */
+      player.webkitRequestFullscreen();
+    } else if (player.msRequestFullscreen) {
+      /* IE11 */
+      player.msRequestFullscreen();
     } else {
-        alert("Please enter a URL first");
-        getVideoURL();
+      alert("Unable to open video in full screen");
     }
+  } else {
+    alert("Please enter a URL first");
+    getVideoURL();
+  }
 }
 
 function refresh() {
-    url = "";
-    document.getElementById("videoPlayer").src = "";
+  url = "";
+  document.getElementById("url").value = "";
+  document.getElementById("videoPlayer").src = "";
+
 }
 
-function shareVideo() {
-    if (videoId != undefined) {
-      navigator.clipboard.writeText("https://youtu.be/ " + videoId);
-      alert("Link copied to clipboard");
+function info() {
+  alert("Welcome to YT-Player! To use YT-Player, click the play icon to play a video, the full screen icon to put the video in full screen, and the reload icon to reset the player if something went wrong.\n\nMade with love by UnrealApex\nThank you to all those who gave feedback and helped improve this project");
+}
+
+function help() {
+  alert("Welcome to YT-Player! To use YT-Player, click the play icon to play a video, the full screen icon to put the video in full screen, and the reload icon to reset the player if something went wrong.");
+}
+
+
+// Get the modal
+var modal = document.getElementById("modal");
+
+// Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var x = document.getElementById("close");
+
+
+// When the user clicks on the button, open the modal
+/* btn.onclick = function() {
+	if (player.src.length != 0) {
+  	modal.style.display = "block";
     } else {
-      alert("Play a video before trying to share\nTry Again");
+    	alert("Enter a url first");
       getVideoURL();
+     
     }
-  }
-  
+}
+*/
+
+// When the user clicks on (x), close the modal
 
 x.onclick = function() {
   modal.style.display = "none";
@@ -88,12 +117,5 @@ window.onclick = function(event) {
   if (event.target == modal) {
 
   }
-  
-function info() {
-    alert("Welcome to YT-Player! To use YT-Player, click the play icon to play a video, the full screen icon to put the video in full screen, and the reload icon to reset the player if something went wrong.\n\nMade with love by UnrealApex\nThank you to all those who gave feedback and helped improve this project");
-}
-
-function help() {
-    alert("Welcome to YT-Player! To use YT-Player, click the play icon to play a video, the full screen icon to put the video in full screen, and the reload icon to reset the player if something went wrong.");
 }
 
