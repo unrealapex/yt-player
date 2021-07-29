b5252723de6c966b836ea4fb018360d0d00d45ab
Aaron <79121360+UnrealApex@users.noreply.github.com>
2021-07-28 19:11:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ function openFullscreen() {
 function refresh() {
   // allows the user to reset the player if they entered an invalid url or ran into another problem
   url = "";
-  document.getElementById("videoPlayer").src = null;
+  document.getElementById("videoPlayer").src = "";
   isLoaded = false;
   return isLoaded;
 }
