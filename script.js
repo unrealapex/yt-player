6e8f23fdbe62afe0fedb45e5d70c65674a202ce6
Aaron <79121360+UnrealApex@users.noreply.github.com>
2021-11-12 23:20:31 -0800

    Input field is focused on hover

--- script.js
+++ script.js
@@ -336,6 +336,11 @@ document.addEventListener("mouseover", function () {
   window.focus();
 });
 
+// focus input field when it is hovered over
+inputField.addEventListener("mouseover", function () {
+  inputField.focus();
+});
+
 // hide the loader every time a video loads in the iframe
 iframe.addEventListener("load", function () {
   loader.classList.add("hidden");
