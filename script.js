a93b2dba5a007102f42e4af7a38efb7147c5630f
Aaron <79121360+UnrealApex@users.noreply.github.com>
2022-04-15 09:14:44 -0700

    Removed legacy code

--- script.js
+++ script.js
@@ -708,53 +708,6 @@ $(function() {
     });
   }
 
-  // // when dragging the queue list, scroll(on the x axis); controls dragging behavior of queue list
-  // document.on("DOMContentLoaded", function () {
-  //   const ele = $queueList;
-  //   ele.css("cursor", "grab");
-
-  //   let pos = { top: 0, left: 0, x: 0, y: 0 };
-
-  //   const mouseDownHandler = function (e) {
-  //     ele.css("cursor", "grabbing");
-  //     ele.css("userSelect", "none");
-
-  //     pos = {
-  //       left: ele.scrollLeft,
-  //       top: ele.scrollTop,
-  //       // Get the current mouse position
-  //       x: e.clientX,
-  //       y: e.clientY
-  //     };
-
-  //     document.on("mousemove", mouseMoveHandler);
-  //     document.on("mouseup", mouseUpHandler);
-  //   };
-
-  //   const mouseMoveHandler = function (e) {
-  //     // How far the mouse has been moved
-  //     const dx = e.clientX - pos.x;
-  //     const dy = e.clientY - pos.y;
-
-  //     // Scroll the element
-  //     ele.scrollTop = pos.top - dy;
-  //     ele.scrollLeft = pos.left - dx;
-  //   };
-
-  //   const mouseUpHandler = function () {
-  //     ele.css("cursor",  "grab");
-  //     ele.css("",emoveProperty("user-select"));
-
-  //     document.off("mousemove", mouseMoveHandler);
-  //     document.off("mouseup", mouseUpHandler);
-  //   };
-
-  //   // Attach the handler
-  //   ele.on("mousedown", mouseDownHandler);
-  // });
-
-
-
   // event listeners
 
   $playButton.on("click", function () {
@@ -813,15 +766,5 @@ $(function() {
     $(this).hide();
     $overlay.show();
   });
-
-  // $("#").on("click", function () {});
-
-  // $("#").on("click", function () {});
-
-  // $("#").on("click", function () {});
-
-  // $("#").on("click", function () {});
-
-  // $("#").on("click", function () {});
 
 });
